5a3290a9630e0c501e048ce0025aa830e8060ecc
dongli <chanxindiaolong@gmail.com>
2019-01-12 15:03:00 +0800

    taotao-search

---
+++ taotao-search-service/src/main/java/com/taotao/search/service/impl/SearchItemServiceImpl.java
@@ -0,0 +1,45 @@
+package com.taotao.search.service.impl;
+
+import java.util.List;
+
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.common.SolrInputDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.taotao.common.pojo.SearchItem;
+import com.taotao.common.utils.TaotaoResult;
+import com.taotao.search.mapper.SearchItemMapper;
+import com.taotao.search.service.SearchItemService;
+
+public class SearchItemServiceImpl implements SearchItemService {
+
+	@Autowired
+	private SearchItemMapper searchItemMapper;
+	@Autowired
+	private SolrServer solrServer;
+
+	@Override
+	public TaotaoResult importItemsToIndex() {
+
+		try {
+
+			List<SearchItem> itemList = searchItemMapper.getItemList();
+			for (SearchItem searchItem : itemList) {
+				SolrInputDocument document = new SolrInputDocument();
+				document.addField("id", searchItem.getId());
+				document.addField("item_title", searchItem.getTitle());
+				document.addField("item_sell_point", searchItem.getSell_point());
+				document.addField("item_price", searchItem.getPrice());
+				document.addField("item_category_name", searchItem.getCategory_name());
+				document.addField("item_desc", searchItem.getItem_desc());
+				solrServer.add(document);
+			}
+			solrServer.commit();
+			return TaotaoResult.ok();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return TaotaoResult.build(500, "数据导入失败");
+		}
+	}
+
+}
